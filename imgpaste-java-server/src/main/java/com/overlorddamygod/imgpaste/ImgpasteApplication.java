28e306e77fd362dcff966ce873d47dd46efc228b
overlorddamygod <overlord.damygod@gmail.com>
2025-08-20 11:43:36 +0545

    add: java server

---
+++ imgpaste-java-server/src/main/java/com/overlorddamygod/imgpaste/ImgpasteApplication.java
@@ -0,0 +1,13 @@
+package com.overlorddamygod.imgpaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgpasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgpasteApplication.class, args);
+	}
+
+}
